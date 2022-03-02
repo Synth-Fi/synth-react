e6c43c1f7ced1d7a7bcdf84b0dc855b5b81953cd
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-03-02 12:23:38 -0800

    base nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Logo from './Logo';
 import '../index.css';
+// import { ReactComponent as Logo } from '../../public/synth-logo.svg';
 const nav_items = [
     {
         name: 'Home',
@@ -17,15 +19,19 @@ const nav_items = [
 
 export default function Navbar() {
     return (
-        <div className="p-2 bg-dark" >
-        <div className="text-yellow text-3xl">
-            dddddddddddd
+        <div className="grid grid-cols-3" >
+        {/* NAV BAR LOGO  */}
+        <Logo/>
+        {/* NAV BAR ITEMS */}
+        <div className="flex flex-row p-3 justify-end">
             {nav_items.map(item => (
-                <div className="">
+                <div className="box-content h-10 w-40 m-3 text-xl bg-nav-img object-contain content-center">
                     <a href={item.path}>{item.name}</a>
                 </div>
             ))}
         </div>
+        {/* OPEN APP BUTTON */}
+        <button>Connect</button>
         </div>
     );
 }
