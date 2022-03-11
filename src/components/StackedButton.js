9849aff52f2c08718fa3e5ed6b4f5dc31ac46d9c
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-03-10 18:50:04 -0800

    update margins

--- src/components/StackedButton.js
+++ src/components/StackedButton.js
@@ -1,9 +1,8 @@
 import React from "react";
-import OutlinedBox from "./OutlinedBox";
 
 export default function StackedButton(props) {
     return (
-        <div className="text-4xl mt-3">
+        <div className="text-4xl mt-12">
             <div className="bg-yellow rounded-2xl stack-item  hover:bg-opacity-70 ">
                 <div className="bg-blue stack-item  hover:bg-opacity-70">
                     <div className="bg-purple stack-item text-center  hover:bg-opacity-70">
