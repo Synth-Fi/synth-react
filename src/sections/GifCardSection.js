c605f127c14de2bfcec8e92c2214a208ae8fcedb
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-03-10 18:54:45 -0800

    fix image formating on gif section

--- src/sections/GifCardSection.js
+++ src/sections/GifCardSection.js
@@ -4,10 +4,10 @@ import OutlinedBox from "../components/OutlinedBox";
 
 export default function GifCardSection(props) {
     return (
-        <div className="bg-card-gif bg-contain p-4 bg-right items-center">
-            {props.outlined_boxes.map(box => {
+        <div className="bg-card-gif bg-contain bg-no-repeat p-4 bg-right items-center">
+            {props.outlined_boxes.map((box,i) => {
                 return (
-                    <OutlinedBox {...box} />
+                    <OutlinedBox {...box} i={i} />
                 );
             })}
             </div>
