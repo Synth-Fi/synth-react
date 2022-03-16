e73a8ff5705e67121b9f1ee353d7a18c4e37ec2e
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-03-15 18:12:22 -0700

    scroll go brrr

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 import StackedButton from '../components/StackedButton';
 export default function HeroSection(props) {
     return (
-            <div className="bg-hero-img bg-cover bg-top bg-no-repeat p-2 min-h-screen">
+            <div className="bg-hero-img bg-cover bg-top bg-no-repeat p-2 min-h-screen bg-fixed">
             <div className="text-left text-6xl p-4 m-5 py-8 max-w-5xl text-white font-press-start">
                     {props.main}
             <StackedButton text={props.button_text} onClick=''/>
