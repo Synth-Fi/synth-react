096b3bc4e28eb6d3e3b2783a9fa6ecd1bd65ec1e
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-03-16 11:54:21 -0700

    app.js is now the pool trading page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 // import logo from './logo.svg';
 import React from "react";
-import HeroSection from "./sections/HeroSection";
-import HeroSection2 from "./sections/HeroSection2";
-import OutlinedBox from "./components/OutlinedBox";
-import StackSection from './sections/StackSection';
-import TeamSection from './sections/TeamSection';
-// import { CryptoCards, Button } from 'web3uikit';
-import { home_page_text } from "./home_page_text";
-import GifCardSection from './sections/GifCardSection';
-
+import PoolPage from "./pages/PoolPage";
 function App() {
   // HOME PAGE
   return (
-    <div className="items-center">
-      <HeroSection {...home_page_text.hero_section1} />
-
-      {/* STACK SECTION */}
-      <div className="flex flex-row justify-center items-center ">
-        <StackSection {...home_page_text.stack_section} />
-      </div>
-      {/* Hero section 2 */}
-      <div className="flex flex-row justify-center items-center ">
-        <HeroSection2 {...home_page_text.hero_section2} />
-      </div>
-      <GifCardSection {...home_page_text} />
-        <TeamSection team={home_page_text.team} />
+    <div className="p-5">
+      <PoolPage/>
     </div>
   );
 }
